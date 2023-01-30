32f7321f87c147bac7f62c83a134242390c19724
Jaime Pillora <dev@jpillora.com>
2023-01-30 14:31:38 +1100

    updated

--- .github/instantiate.sh
+++ .github/instantiate.sh
@@ -3,8 +3,8 @@
 echo "$SSH_KEY" >/tmp/private.key
 chmod 600 /tmp/private.key
 # instantiate this template with demo repo "go-template-demo"
-set -x
-
+set -euf -o pipefail
+# install dependencies
 cd /tmp
 curl 'https://i.jpillora.com/BurntSushi/ripgrep!?as=rg' | bash
 
@@ -17,20 +17,26 @@ pwd
 rm -rf $GITHUB_WORKSPACE/demo/*
 # replace with template
 cp -r $GITHUB_WORKSPACE/template/root/. $GITHUB_WORKSPACE/demo
-# swap placeholders
-# sed -i "s/GO_TEMPLATE_DEMO/$REPO_NAME/g" README.md
 cd $GITHUB_WORKSPACE/demo
 pwd
 ls -lah
 
+/usr/local/bin/rg --help
 /usr/local/bin/rg myuser
 /usr/local/bin/rg myrepo
 
-go mod init
+# swap placeholders
+# sed -i "s/GO_TEMPLATE_DEMO/$REPO_NAME/g" README.md
 
+# confirm we can build
+go mod init github.com/jpillora/go-template-demo
+go build -v -o /dev/null .
+
+# mark as generated
 echo "generated this repo at: $(date)" >generated.txt
 git config user.name go-template
 git config user.email jpillora@users.noreply.github.com
 git add .
 git commit -m "updated"
+# push
 git -c core.sshCommand="ssh -i /tmp/private.key" push
