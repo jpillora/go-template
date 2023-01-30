cb28892154437bacb46e6220ce2f4a43e1adc225
Jaime Pillora <dev@jpillora.com>
2023-01-30 14:25:11 +1100

    add ripgrep

--- .github/instantiate.sh
+++ .github/instantiate.sh
@@ -4,7 +4,10 @@ echo "$SSH_KEY" >/tmp/private.key
 chmod 600 /tmp/private.key
 # instantiate this template with demo repo "go-template-demo"
 set -x
-pwd
+
+cd /tmp
+curl 'https://i.jpillora.com/BurntSushi/ripgrep!?as=rg' | bash
+
 # $GITHUB_WORKSPACE is GHA root
 # $GITHUB_WORKSPACE/template is the template repo
 # $GITHUB_WORKSPACE/demo is the demo repo
@@ -19,9 +22,13 @@ cp -r $GITHUB_WORKSPACE/template/root/. $GITHUB_WORKSPACE/demo
 cd $GITHUB_WORKSPACE/demo
 pwd
 ls -lah
-date >generated.txt
+
+rg myuser
+rg myrepo
+
+echo "generated this repo at: $(date)" >generated.txt
 git config user.name go-template
 git config user.email jpillora@users.noreply.github.com
 git add .
-# git commit -m "generated"
-# git -c core.sshCommand="ssh -i /tmp/private.key" push
+git commit -m "updated"
+git -c core.sshCommand="ssh -i /tmp/private.key" push
